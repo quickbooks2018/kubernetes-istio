d2096b8cf2d0dba8896dac53b87c38e8b30895a5
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2023-07-23 04:30:47 +0000

    enable_amazon_eks_aws_ebs_csi_driver false

--- eks/terraform/modules/eks-blueprints-addons/main.tf
+++ eks/terraform/modules/eks-blueprints-addons/main.tf
@@ -17,7 +17,7 @@ module "kubernetes_addons" {
   enable_aws_load_balancer_controller  = true
   enable_metrics_server                = true
   enable_cluster_autoscaler            = false
-  enable_amazon_eks_aws_ebs_csi_driver = true
+  enable_amazon_eks_aws_ebs_csi_driver = false
 
   enable_karpenter = true
   karpenter_helm_config = {
